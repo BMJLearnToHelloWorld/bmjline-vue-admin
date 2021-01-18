ca3c90998e509930760fce1c232786080817bd0f
BMJ <BernieMJ1012@gmail.com>
2021-01-19 00:44:46 +0800

    update tinymce component and add to blog edit page

---
+++ src/components/tinymce/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="Tinymce">
+    <editor
+      v-model="tinymceValue"
+      api-key="no-api-key"
+      :init="init"
+      :disable="disabled"
+    />
+  </div>
+</template>
+
+<script>
+import tinymce from 'tinymce/tinymce'
+import Editor from '@tinymce/tinymce-vue'
+
+export default {
+  name: 'Tinymce',
+  components: {
+    editor: Editor
+  },
+  props: {
+    // 传入一个value，使组件支持v-model绑定
+    value: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    plugins: {
+      type: [String, Array],
+      default:
+        'code lists image media table textcolor colorpicker wordcount contextmenu emoticons link insertdatetime charmap fullscreen searchreplace'
+    },
+    toolbar: {
+      type: [String, Array],
+      default:
+        'newdocument code | undo redo | formatselect visualaid | fontsizeselect | bold italic underline strikethrough forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent blockquote | lists image media table link unlink | emoticons insertdatetime charmap | fullscreen searchreplace | removeformat'
+    }
+  },
+  data() {
+    return {
+      init: {
+        // language_url: '/public/tinymce/js/tinymce/langs/zh_CN.js',
+        // language: 'zh_CN',
+        // skin_url: '/tinymce/skins/ui/oxide', // 主题样式
+        height: 600,
+        menubar: false,
+        plugins: this.plugins,
+        toolbar: this.toolbar,
+        browser_spellcheck: true, // 拼写检查
+        statusbar: true, // 隐藏编辑器底部的状态栏
+        paste_data_images: true, // 允许粘贴图像
+        images_upload_handler: function(blobInfo, success, failure) {
+          console.log(blobInfo.filename())
+        }
+      },
+      tinymceValue: this.value
+    }
+  },
+  watch: {
+    value(newValue) {
+      this.tinymceValue = newValue
+    },
+    tinymceValue(newValue) {
+      this.$emit('input', newValue)
+    }
+  },
+  mounted() {
+    tinymce.init({})
+  },
+  methods: {
+    // 添加相关的事件，可用的事件参照文档=> https://github.com/tinymce/tinymce-vue => All available events
+    // 需要什么事件可以自己增加
+    onClick(e) {
+      this.$emit('onClick', e, tinymce)
+    },
+    // 可以添加一些自己的自定义事件，如清空内容
+    clear() {
+      this.tinymceValue = ''
+    }
+  }
+}
+</script>
--- src/views/blog/edit.vue
+++ src/views/blog/edit.vue
@@ -1,78 +1,54 @@
 <template>
   <div class="app-container">
-    <el-input v-model="filterText" placeholder="Filter keyword" style="margin-bottom:30px;" />
-
-    <el-tree
-      ref="tree2"
-      :data="data2"
-      :props="defaultProps"
-      :filter-node-method="filterNode"
-      class="filter-tree"
-      default-expand-all
-    />
-
+    <tinymce-editor ref="tinymceEditor" v-model="editorValue" />
+    <el-row class="editor_btn_row">
+      <el-button type="danger" round @click="clearContent">清空</el-button>
+      <el-button type="primary" round :loading="loadingSaveBtn" @click="saveContent">保存</el-button>
+      <el-button type="success" round :loading="loadingPublishBtn" @click="publishContent">保存并发布</el-button>
+    </el-row>
+    {{ editorValue }}
   </div>
 </template>
 
 <script>
+import TinymceEditor from '@/components/tinymce/index'
 export default {
-
+  components: {
+    TinymceEditor
+  },
   data() {
     return {
-      filterText: '',
-      data2: [{
-        id: 1,
-        label: 'Level one 1',
-        children: [{
-          id: 4,
-          label: 'Level two 1-1',
-          children: [{
-            id: 9,
-            label: 'Level three 1-1-1'
-          }, {
-            id: 10,
-            label: 'Level three 1-1-2'
-          }]
-        }]
-      }, {
-        id: 2,
-        label: 'Level one 2',
-        children: [{
-          id: 5,
-          label: 'Level two 2-1'
-        }, {
-          id: 6,
-          label: 'Level two 2-2'
-        }]
-      }, {
-        id: 3,
-        label: 'Level one 3',
-        children: [{
-          id: 7,
-          label: 'Level two 3-1'
-        }, {
-          id: 8,
-          label: 'Level two 3-2'
-        }]
-      }],
-      defaultProps: {
-        children: 'children',
-        label: 'label'
-      }
+      editorValue: '',
+      loadingClearBtn: false,
+      loadingSaveBtn: false,
+      loadingPublishBtn: false,
+      blogId: ''
     }
   },
-  watch: {
-    filterText(val) {
-      this.$refs.tree2.filter(val)
-    }
+  created() {
+    this.getBLogId()
   },
-
   methods: {
-    filterNode(value, data) {
-      if (!value) return true
-      return data.label.indexOf(value) !== -1
+    getBLogId() {
+      console.log(this.$route.path)
+      this.blogId = this.$route.path.id
+    },
+    clearContent() {
+      this.$refs.tinymceEditor.clear()
+    },
+    saveContent() {
+      console.log(this.editorValue)
+    },
+    publishContent() {
+      console.log(this.editorValue)
     }
   }
 }
 </script>
 
+<style lang="scss" scpoed>
+.editor_btn_row {
+  text-align: center;
+  margin-top: 1rem;
+}
+</style>
