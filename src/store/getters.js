53a027bb884ae2efa428aabed104b3b28d7f681e
BMJ <BernieMJ1012@gmail.com>
2021-02-04 10:51:36 +0800

    add tagsView

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,10 +1,13 @@
 const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
+  visitedViews: state => state.tagsView.visitedViews,
+  cachedViews: state => state.tagsView.cachedViews,
   token: state => state.user.token,
   avatar: state => state.user.avatar,
   username: state => state.user.username,
   introduction: state => state.user.introduction,
-  roles: state => state.user.roles
+  roles: state => state.user.roles,
+  permission_routes: state => state.permission.routes
 }
 export default getters
